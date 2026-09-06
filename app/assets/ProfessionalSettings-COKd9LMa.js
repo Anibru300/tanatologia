58c3fbd8a49f1fe995a98f37d52874a24678cfe5
Anibru300 <255746863+Anibru300@users.noreply.github.com>
2026-09-06 23:23:24 +0000

    chore: build automático de /app

---
+++ app/assets/ProfessionalSettings-COKd9LMa.js
@@ -0,0 +1,1 @@
+import{t as e}from"./jsx-runtime-CsLdoyK9.js";import{t}from"./AccountSettings-Cf_8-RMz.js";var n=e();function r(){return(0,n.jsx)(t,{profilePath:`/app/#/profesional/perfil`,profileLabel:`Mi perfil`})}export{r as ProfessionalSettings};
