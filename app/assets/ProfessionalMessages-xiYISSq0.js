f1c41645c061704a4007c4b05d3683b2dded78ad
Anibru300 <255746863+Anibru300@users.noreply.github.com>
2026-09-06 18:28:24 +0000

    chore: build automático de /app

--- app/assets/ProfessionalMessages-xiYISSq0.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./jsx-runtime-CsLdoyK9.js";import{t}from"./MessagesInterim-BkKTh5L9.js";var n=e();function r(){return(0,n.jsx)(t,{audience:`professional`,appointmentsTo:`/profesional/agenda`,appointmentsLabel:`Ir a mi agenda`})}export{r as ProfessionalMessages};
